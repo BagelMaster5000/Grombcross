85989aa17b4e65a2d703fbaced0b07614c459781
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-08-03 20:01:19 -0500

    Added transitions

--- Grombcross/MainWindow.xaml.cs
+++ Grombcross/MainWindow.xaml.cs
@@ -1,28 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Grombcross.ViewModels;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
-namespace Grombcross
-{
+namespace Grombcross {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        MainViewModel mainViewModel;
+        private Duration transitionInDuration;
+        private Duration transitionOutDuration;
+
+        public MainWindow() {
+            Loaded += OnLoaded;
+
             InitializeComponent();
+
+            TransitionOverlay.Opacity = 0;
+            TransitionOverlay.IsHitTestVisible = false;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            mainViewModel = DataContext as MainViewModel;
+
+
+            transitionInDuration = new Duration(TimeSpan.FromSeconds(mainViewModel.TransitionInDuration));
+            transitionOutDuration = new Duration(TimeSpan.FromSeconds(mainViewModel.TransitionOutDuration));
+
+            mainViewModel.OnTransitionIn += TransitionInAnimation;
+            mainViewModel.OnTransitionOut += TransitionOutAnimation;
+            mainViewModel.OnTransitionFinished += TransitionFinished;
+        }
+
+        private void TransitionInAnimation() {
+            DoubleAnimation opacityAnimation = new DoubleAnimation(1, transitionInDuration);
+            TransitionOverlay.BeginAnimation(OpacityProperty, opacityAnimation);
+            TransitionOverlay.IsHitTestVisible = true;
+        }
+
+        private void TransitionOutAnimation() {
+            DoubleAnimation opacityAnimation = new DoubleAnimation(0, transitionOutDuration);
+            TransitionOverlay.BeginAnimation(OpacityProperty, opacityAnimation);
+        }
+
+        private void TransitionFinished() {
+            TransitionOverlay.IsHitTestVisible = false;
         }
     }
 }
