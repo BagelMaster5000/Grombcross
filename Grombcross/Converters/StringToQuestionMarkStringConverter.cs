84959e22ff7e7d78858690b4bf3b02b057565ac8
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-08-16 20:20:42 -0500

    Incomplete puzzles now show hidden puzzle name with question marks

---
+++ Grombcross/Converters/StringToQuestionMarkStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Grombcross.Converters {
+    [ValueConversion(typeof(string), typeof(string))]
+    public class StringToQuestionMarkStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string originalString = (string)value;
+
+            string questionString = new String(originalString.Select(c => c == ' ' ? ' ' : '?').ToArray());
+            return questionString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
